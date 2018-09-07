424df5571a408278767efc19ce798ad6136213c8
Xing-Wei Ho <ho861206@gmail.com>
2018-09-07 12:46:18 +0800

    Add files via upload

---
+++ Example/EXP 009 - For.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+ 
+int main()
+{
+	for(int i = 1; i <= 10; i++)
+	{
+		printf("Case %d: Hello!\n", i);
+	}
+	return 0;
+} 
---
+++ Example/EXP 010 - DoWhile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num = 1;
+	do
+	{
+		printf("Case %d: Hello!\n", num++);
+	} while( num <= 10 );
+	
+	return 0;
+}
