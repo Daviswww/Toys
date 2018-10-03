46b4832ff7ab92c759e137fa68860826edb91d54
Xing-Wei Ho <ho861206@gmail.com>
2018-10-03 15:40:50 +0800

    Add files via upload

---
+++ Python Programming/EXC 014 - String IV.py
@@ -0,0 +1,6 @@
+str = 'apple is a kind of fruit, apple is a brand of cell phone'
+print(str.replace(',', ''))
+print(str.split(' '))
+print(type(str.split(' ')))
+print(str.split(' ')[0])
+print(str.count(str.split(' ')[0]))
