589bd70077b6f6c7e6e719d7a6c4fff75610415b
Xing-Wei Ho <ho861206@gmail.com>
2018-10-31 23:53:14 +0800

    Delete EXC 013 - String III.py

--- Python Programming/EXC 013 - String III.py
+++
@@ -1,8 +0,0 @@
-line1 = 'Welcome to Python World Game.'
-print(line1.split())
-line2 = 'Can you can a can as a canner can can a can?'
-print(line2.count('a'))
-line3 = 'Few free fruit flies fly from flames.'
-print(line3.index('fly'))
-line4 = 'Czngrxtulxtizns, yzu hxve pxssed the czmpetitizn.'
-print(line4.replace('z', 'o')) 
