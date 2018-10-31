070888aa25c8393a9422dca897df2e2d1ae33698
Xing-Wei Ho <ho861206@gmail.com>
2018-10-31 23:53:19 +0800

    Delete EXC 014 - String IV.py

--- Python Programming/EXC 014 - String IV.py
+++
@@ -1,6 +0,0 @@
-str = 'apple is a kind of fruit, apple is a brand of cell phone'
-print(str.replace(',', ''))
-print(str.split(' '))
-print(type(str.split(' ')))
-print(str.split(' ')[0])
-print(str.count(str.split(' ')[0]))
